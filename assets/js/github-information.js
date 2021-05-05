fbe2d3a58c0f76ba7da332985f819b53408730c2
tony-reddington <tony.reddington@gmail.com>
2021-05-05 13:05:50 +0000

    Fix bugs for empty to empty divs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,6 +37,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -70,4 +72,6 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
 
